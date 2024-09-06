70aa3e29bfdb1c63830737fa7fd99beb9afe10f1
Yuki Nagae yuki.nagae <yuki.nagae1130@gmail.com>
2024-09-06 11:37:50 +0900

    remove comments and unused code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,16 @@
 import * as z from 'zod'
-
-// Import the Genkit core libraries and plugins.
 import { generate } from '@genkit-ai/ai'
 import { configureGenkit } from '@genkit-ai/core'
-import { defineFlow, startFlowsServer } from '@genkit-ai/flow'
+import { defineFlow } from '@genkit-ai/flow'
 import { googleAI } from '@genkit-ai/googleai'
-
-// Import models from the Google AI plugin. The Google AI API provides access to
-// several generative models. Here, we import Gemini 1.5 Flash.
 import { gemini15Flash } from '@genkit-ai/googleai'
 
 configureGenkit({
-  plugins: [
-    // Load the Google AI plugin. You can optionally specify your API key
-    // by passing in a config object; if you don't, the Google AI plugin uses
-    // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
-    // the recommended practice.
-    googleAI(),
-  ],
-  // Log debug output to tbe console.
+  plugins: [googleAI()],
   logLevel: 'debug',
-  // Perform OpenTelemetry instrumentation and enable trace collection.
   enableTracingAndMetrics: true,
 })
 
-// Define a simple flow that prompts an LLM to generate menu suggestions.
 export const menuSuggestionFlow = defineFlow(
   {
     name: 'menuSuggestionFlow',
@@ -32,7 +18,6 @@ export const menuSuggestionFlow = defineFlow(
     outputSchema: z.string(),
   },
   async (subject) => {
-    // Construct a request and send it to the model API.
     const llmResponse = await generate({
       prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
       model: gemini15Flash,
@@ -41,15 +26,6 @@ export const menuSuggestionFlow = defineFlow(
       },
     })
 
-    // Handle the response from the model API. In this sample, we just convert
-    // it to a string, but more complicated flows might coerce the response into
-    // structured output or chain the response into another LLM call, etc.
     return llmResponse.text()
   },
 )
-
-// Start a flow server, which exposes your flows as HTTP endpoints. This call
-// must come last, after all of your plug-in configuration and flow definitions.
-// You can optionally specify a subset of flows to serve, and configure some
-// HTTP server options, but by default, the flow server serves all defined flows.
-startFlowsServer()
